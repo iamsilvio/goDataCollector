33d37a0fd1f84cdd7d0a04bf15c2b9a8389c0d95
Silvio <silvio@deleteonerror.com>
2020-08-08 15:11:33 +0200

    h: temporary fix to use insecure tls #Bug

--- data/influxDb.go
+++ data/influxDb.go
@@ -13,7 +13,6 @@ var roots *x509.CertPool
 
 func SetConfig(conf InfluxDbConfig) {
 	config = conf
-
 	roots = x509.NewCertPool()
 
 }
@@ -22,7 +21,8 @@ func Write(data *DataPoint) {
 
 	client := influxdb2.NewClientWithOptions(config.ServerURL, config.UserName+":"+config.Password,
 		influxdb2.DefaultOptions().SetUseGZip(true).SetTlsConfig(&tls.Config{
-			RootCAs: roots}))
+			RootCAs:            roots,
+			InsecureSkipVerify: true}))
 
 	writeApi := client.WriteApiBlocking("", config.Bucket)
 	writeApi.WriteRecord(context.Background(), data.ToLineProtocol())
