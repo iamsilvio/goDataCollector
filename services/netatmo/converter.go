bd54d71f2eea73c1f45ecc8d8d510ae47372c2bc
silvio <silvio@deleteonerror.com>
2023-01-15 19:07:43 +0100

    influx2 has changed type handling 🧐

--- services/netatmo/converter.go
+++ services/netatmo/converter.go
@@ -1,7 +1,6 @@
 package netatmo
 
 import (
-	"fmt"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
@@ -13,12 +12,12 @@ func (net *Dashboard) ToPoint() *write.Point {
 	p := influxdb2.NewPointWithMeasurement("HomeClima").
 		AddTag("Room", "rabbitWarren").
 		AddTag("Service", "NetAtmoAPI").
-		AddField("Temperature", fmt.Sprintf("%f", net.Temperature)).
-		AddField("Co2", fmt.Sprintf("%d", net.Co2)).
-		AddField("Humidity", fmt.Sprintf("%d", net.Humidity)).
-		AddField("Noise", fmt.Sprintf("%d", net.Noise)).
-		AddField("Pressure", fmt.Sprintf("%f", net.Pressure)).
-		AddField("AbsolutePressure", fmt.Sprintf("%f", net.AbsolutePressure)).
+		AddField("Temperature", net.Temperature).
+		AddField("Co2", net.Co2).
+		AddField("Humidity", net.Humidity).
+		AddField("Noise", net.Noise).
+		AddField("Pressure", net.Pressure).
+		AddField("AbsolutePressure", net.AbsolutePressure).
 		SetTime(time.Unix(net.Time, 0))
 
 	return p
